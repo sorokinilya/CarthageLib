4afa81f6b9a610283f94fb903fa3fd0781b4bafc
sorokinilya <ilya.sorokin@sigmaukraine.com>
2018-02-27 16:15:00 +0200

    Add project

---
+++ CarthageLib/CarthageLib/CarthageLib.h
@@ -0,0 +1,19 @@
+//
+//  CarthageLib.h
+//  CarthageLib
+//
+//  Created by Ilya Sorokin on 2/27/18.
+//  Copyright © 2018 Ilya Sorokin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CarthageLib.
+FOUNDATION_EXPORT double CarthageLibVersionNumber;
+
+//! Project version string for CarthageLib.
+FOUNDATION_EXPORT const unsigned char CarthageLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CarthageLib/PublicHeader.h>
+
+
---
+++ CarthageLib/CarthageLib/CarthageVersion.swift
@@ -0,0 +1,15 @@
+//
+//  CarthageVersion.swift
+//  CarthageLib
+//
+//  Created by Ilya Sorokin on 2/27/18.
+//  Copyright © 2018 Ilya Sorokin. All rights reserved.
+//
+
+import UIKit
+
+public class CarthageVersion {
+
+    public static let version = 1
+
+}
